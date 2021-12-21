66149c73ab1749487fb9f50315690890d211dbae
TheAlot <viktorvase@gmail.com>
2021-12-21 01:14:20 +0100

    feat: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from 'react';
+import logo from './assets/logo.svg';
+import './App.css';
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -11,7 +11,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
+          <button type="button" onClick={() => setCount((value) => value + 1)}>
             count is: {count}
           </button>
         </p>
@@ -27,7 +27,7 @@ function App() {
           >
             Learn React
           </a>
-          {" | "}
+          {' | '}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -41,5 +41,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
