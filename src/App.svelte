bac332540b3c48d97748b3a622fe891396be8dcd
TheAlot <viktorvase@gmail.com>
2021-12-22 21:11:04 +0100

    feat: change to svelte framework

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import AwesomeButton from './lib/atoms/AwesomeButton.svelte';
+</script>
+
+<main>
+  <AwesomeButton />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
