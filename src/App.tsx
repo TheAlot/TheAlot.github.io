eb23e3877a6a0bdcf3fac892e335339f99c054a2
TheAlot <viktorvase@gmail.com>
2022-05-20 19:17:33 +0200

    test: add test comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'solid-js';
 import { Navbar } from './components/navbar';
 const App: Component = () => {
+  // test comment
   return (
     <div class="flex">
       <Navbar />
