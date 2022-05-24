3c6b21563e2be6759021c020e1c5842871803ca4
TheAlot <viktorvase@gmail.com>
2022-05-24 09:34:49 +0200

    build: add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
@@ -6,5 +7,11 @@ export default defineConfig({
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
+  },
+  resolve: {
+    alias: {
+      _components: resolve(__dirname, 'src/components'),
+      _pages: resolve(__dirname, 'src/pages'),
+    },
   },
 });
